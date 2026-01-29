a9b21191c7b87d990e152b4d7008b67c97e14aa7
Anthony Stone <stonea13@southernct.edu>
2026-01-29 09:53:47 -0500

    Fineshed Lecture one, added links

--- examples/Example02.py
+++ examples/Example02.py
@@ -1,4 +1,10 @@
-integer = 1 #Data type: int
-boolean = True #Data type: bool
-FloatingPointNumber = 0.1 #Data type: float
-#...
+# Primative Data Type
+print("Im an integer, called ", type(6))
+
+# Abstract Data Type
+print("I want to release data in reverse order of it's arrival")
+
+# Data Structure
+stack = [1, 2, 3]
+stack.append(4)
+print(f"4 comes and goes: {stack.pop()}, leaving {stack}")
--- examples/Example03.py
+++
@@ -1,9 +0,0 @@
-# This is a little function to check the type of a variable
-def seeType(variable):
-    print(type(variable))
-
-integer = 1
-floatPN = 0.1
-boolean = False
-
-seeType(integer) # Play around with it!
