d6dbc4cbba25c7d5cdff1839cff122734a7cb067
Anthony Stone <stonea13@southernct.edu>
2026-02-04 18:06:38 -0500

    Lecture

---
+++ examples/Example03.py
@@ -0,0 +1,14 @@
+numOne = 1
+numTwo = 2
+
+#Check Types
+print(type(numOne))
+print(type(numTwo))
+
+print(type(numOne + numTwo)) # Addition
+print(type(numOne - numTwo)) # Subtraction
+print(type(numOne * numTwo)) # Multiplication
+print(type(numOne // numTwo)) # Integer division
+print(type(numOne % numTwo)) # Modulo division
+
+print(type(numOne / numTwo)) # Devision XX
